9551dd01e71e56ba4beaf9ab211af08eb28d3fd0
David Arevalo <davidarevaloc20@gmail.com>
2021-05-22 12:47:05 -0500

    finish the style

---
+++ src/JavaScript/header.js
@@ -0,0 +1,3 @@
+document.getElementById("buttonMenu").addEventListener("click", ()=>{
+  alert("Si")
+})
---
+++ src/JavaScript/main.js
@@ -0,0 +1,4 @@
+//header
+document.getElementById("buttonMenu").addEventListener("click", ()=>{
+  document.querySelector("#headerNav").classList.toggle("display-None")
+})
